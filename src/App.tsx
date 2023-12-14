e05e91ca3669d249d370509392ab05e940c3de74
gausss <gausssven@gmail.com>
2023-12-14 16:47:52 +0100

    Add meal screen

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,39 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { useColorScheme } from 'react-native';
+import { NativeModules, Platform, useColorScheme } from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
 import Meals from './Meals';
 import Plan from './Plan';
 
-const Tab = createBottomTabNavigator();
 
 export default function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const deviceLang = Platform.select({
+    ios: NativeModules.SettingsManager?.settings?.AppleLocale || NativeModules.SettingsManager?.settings?.AppleLanguages[0],
+    android: NativeModules.I18nManager.localeIdentifier,
+  });
+  const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
       <Tab.Navigator initialRouteName="Plan">
         <Tab.Screen name="Plan" component={Plan} options={{
-          tabBarIcon: ({ focused, color, size }) => {{
-            return <Icon name='calendar-outline' size={size} color={color} />
-          }}
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            {
+              return <Icon name='calendar-outline' size={size} color={color} />
+            }
+          }
         }} />
         <Tab.Screen name="Meals" component={Meals} options={{
-          tabBarIcon: ({ focused, color, size }) => {{
-            return <Icon name='restaurant-outline' size={size} color={color} />
-          }}
-        }}/>
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            {
+              return <Icon name='restaurant-outline' size={size} color={color} />
+            }
+          }
+        }}></Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer >
   );
--- src/Meals.tsx
+++ src/Meals.tsx
@@ -1,11 +1,43 @@
 import { useTheme } from "@react-navigation/native";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { Section } from "./Section";
 
+export interface Meal {
+    name: string,
+    ingredients: string
+}
 
 export default function Meals(): React.JSX.Element {
     const { colors } = useTheme();
+
+    const Stack = createNativeStackNavigator();
+
+    return <Stack.Navigator>
+        <Stack.Screen
+            name="List"
+            component={MealList}
+        />
+        <Stack.Screen name="Add"
+            component={MealAdd}
+        />
+    </Stack.Navigator>
+};
+
+function MealList({ route, navigation }): React.JSX.Element {
+    const { colors } = useTheme();
+
+    React.useEffect(() => {
+        if (route.params?.newMeal) {
+            setMeals([...meals, route.params.newMeal]);
+            route.params.newMeal = undefined;
+            console.log(meals);
+        }
+    }, [route.params?.newMeal]);
+
+    const [meals, setMeals] = useState<Meal[]>([]);
 
     return <View style={{ height: '100%' }}>
         <ScrollView
@@ -17,6 +49,9 @@ export default function Meals(): React.JSX.Element {
                 <Section title="Step One">
                     Add your <Text style={styles.highlight}>favorite</Text> meals to the list.
                 </Section>
+                <View>
+                    {meals.map((value, key) => { return <Text key={key} style={{ color: colors.text }}>{value.name}</Text> })}
+                </View>
                 <View style={{
                     alignItems: 'center',
                     gap: 20
@@ -35,7 +70,7 @@ export default function Meals(): React.JSX.Element {
                         name="add-outline"
                         borderRadius={25}
                         backgroundColor={colors.primary}
-                        onPress={() => console.log("Pressed")}
+                        onPress={() => navigation.navigate('Add')}
                     >
                         Add meal
                     </Icon.Button>
@@ -43,7 +78,45 @@ export default function Meals(): React.JSX.Element {
             </View>
         </ScrollView>
     </View>;
-};
+}
+function MealAdd({ navigation }): React.JSX.Element {
+    const { colors } = useTheme();
+    const [name, setName] = useState('');
+    const [ingredients, setIngredients] = useState('');
+
+    return <ScrollView>
+        <View style={{ height: '100%', padding: 15, gap: 20 }}>
+            <TextInput
+                style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
+                placeholder="Name"
+                onChangeText={name => setName(name)}
+                defaultValue={name}
+            />
+            <TextInput
+                style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
+                placeholder="Ingredients"
+                onChangeText={ingredients => setIngredients(ingredients)}
+                defaultValue={ingredients}
+            />
+            <View style={{ alignItems: 'center' }}>
+                <Icon.Button
+                    style={{ paddingVertical: 15, paddingHorizontal: 25 }}
+                    name="add-outline"
+                    borderRadius={25}
+                    backgroundColor={colors.primary}
+                    onPress={() => navigation.navigate({
+                        name: 'List',
+                        params: { newMeal: { name, ingredients } },
+                        merge: true,
+                    })}
+                >
+                    Save
+                </Icon.Button>
+            </View>
+        </View>
+    </ScrollView >
+}
+
 
 const styles = StyleSheet.create({
     highlight: {
