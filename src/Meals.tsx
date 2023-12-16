aaf3a018e624ebcc78a86521cac1a0416b440880
gausss <gausssven@gmail.com>
2023-12-16 12:04:10 +0100

    Add ids and inital meal list

--- src/Meals.tsx
+++ src/Meals.tsx
@@ -2,15 +2,17 @@ import { useTheme } from "@react-navigation/native";
 import { NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Image, ScrollView, Text, TextInput, View } from "react-native";
+import { FlatList, Image, Text, TextInput, View } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import Icon from "react-native-vector-icons/Ionicons";
 import { Section } from "./Section";
+import { IdGenerator } from "./components/IdGenerator";
 
 export interface Meal {
+    id: string,
     name: string,
     ingredients: Ingredient[],
-    complex?: Complexity
+    complexity?: Complexity
 }
 
 type Complexity = 'EASY' | 'HARD';
@@ -65,44 +67,44 @@ function MealList({ route, navigation }: ListProps): React.JSX.Element {
 
     const [meals, setMeals] = useState<Meal[]>([]);
 
-    return <View style={{ height: '100%' }}>
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic">
-            <View
+    return <View
+        style={{
+            backgroundColor: colors.background,
+        }}>
+        <Section title={t('meals.introHeading')}>
+            <Text>{t('meals.introDescription')}</Text>
+        </Section>
+        <FlatList
+            data={meals}
+            renderItem={({ item, index, separators }) => (
+                <View style={{ height: 50, backgroundColor: colors.card, borderRadius: 12, padding: 15 }}>
+                    <Text style={{ color: colors.text, fontSize: 14 }}>{item.name}</Text>
+                </View>
+            )}
+        />
+        <View style={{
+            alignItems: 'center',
+            gap: 20
+        }}>
+            <Image
+                source={require('./img/Ravioli.png')}
                 style={{
-                    backgroundColor: colors.background,
-                }}>
-                <Section title={t('meals.introHeading')}>
-                    <Text>{t('meals.introDescription')}</Text>
-                </Section>
-                <View>
-                    {meals.map((value, key) => { return <Text key={key} style={{ color: colors.text }}>{value.name}</Text> })}
-                </View>
-                <View style={{
-                    alignItems: 'center',
-                    gap: 20
-                }}>
-                    <Image
-                        source={require('./img/Ravioli.png')}
-                        style={{
-                            height: 380,
-                            resizeMode: 'contain',
-                            tintColor: colors.text,
-                        }}
-                    />
-                    <Icon.Button
-                        style={{ paddingVertical: 15, paddingHorizontal: 25 }}
-                        name="add-outline"
-                        borderRadius={25}
-                        backgroundColor={colors.primary}
-                        onPress={() => navigation.navigate('Add')}
-                    >
-                        {t('meals.add')}
-                    </Icon.Button>
-                </View>
-            </View>
-        </ScrollView>
-    </View>;
+                    height: 380,
+                    resizeMode: 'contain',
+                    tintColor: colors.text,
+                }}
+            />
+            <Icon.Button
+                style={{ paddingVertical: 15, paddingHorizontal: 25 }}
+                name="add-outline"
+                borderRadius={25}
+                backgroundColor={colors.primary}
+                onPress={() => navigation.navigate('Add')}
+            >
+                {t('meals.add')}
+            </Icon.Button>
+        </View>
+    </View>
 }
 function MealAdd({ navigation }: AddProps): React.JSX.Element {
     const { colors } = useTheme();
@@ -112,59 +114,57 @@ function MealAdd({ navigation }: AddProps): React.JSX.Element {
     const [ingredients, setIngredients] = useState([]);
     const [complex, setComplex] = useState<Complexity>();
 
-    return <ScrollView>
-        <View style={{ height: '100%', padding: 15, gap: 20 }}>
-            <TextInput inputMode='text' clearButtonMode='always'
-                placeholderTextColor='grey'
-                style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
-                placeholder={t('meals.name')}
-                onChangeText={name => setName(name)}
-                defaultValue={name}
-            />
-            <TextInput inputMode='text' clearButtonMode='always'
-                placeholderTextColor='grey'
-                style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
-                placeholder={t('meals.ingredients')}
-            // onChangeText={ingredients => setIngredients(ingredients)}
-            // defaultValue={ingredients}
-            />
-            <SelectDropdown
-                defaultButtonText={t('meals.complexity.name')}
-                buttonTextStyle={{ textAlign: 'left', color: complex ? colors.text : 'grey', fontSize: 14 }}
-                buttonStyle={{ height: 50, backgroundColor: colors.card, borderRadius: 12, padding: 15, width: '100%' }}
-                dropdownOverlayColor='transparent'
-                rowTextStyle={{ color: colors.text, textAlign: 'left', fontSize: 14 }}
-                selectedRowTextStyle={{ color: colors.primary }}
-                dropdownStyle={{ backgroundColor: colors.card, borderRadius: 12, padding: 15, height:'auto' }}
-                renderDropdownIcon={() => <Icon name="chevron-down-outline" color={colors.text}></Icon>}
-                dropdownIconPosition='right'
-                data={['EASY', 'HARD']}
-                rowTextForSelection={(item) => {
-                    return t(`meals.complexity.${item}`)
-                }}
-                buttonTextAfterSelection={(item) => {
-                    return t(`meals.complexity.${item}`)
-                }}
-                onSelect={(selectedItem) => {
-                    setComplex(selectedItem);
-                }}
-            />
+    return <View style={{ height: '100%', padding: 15, gap: 20 }}>
+        <TextInput inputMode='text' clearButtonMode='always'
+            placeholderTextColor='grey'
+            style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
+            placeholder={t('meals.name')}
+            onChangeText={name => setName(name)}
+            defaultValue={name}
+        />
+        <TextInput inputMode='text' clearButtonMode='always'
+            placeholderTextColor='grey'
+            style={{ height: 50, color: colors.text, backgroundColor: colors.card, borderRadius: 12, padding: 15, fontSize: 14 }}
+            placeholder={t('meals.ingredients')}
+        // onChangeText={ingredients => setIngredients(ingredients)}
+        // defaultValue={ingredients}
+        />
+        <SelectDropdown
+            defaultButtonText={t('meals.complexity.name')}
+            buttonTextStyle={{ textAlign: 'left', color: complex ? colors.text : 'grey', fontSize: 14 }}
+            buttonStyle={{ height: 50, backgroundColor: colors.card, borderRadius: 12, padding: 15, width: '100%' }}
+            dropdownOverlayColor='transparent'
+            rowTextStyle={{ color: colors.text, textAlign: 'left', fontSize: 14 }}
+            selectedRowTextStyle={{ color: colors.primary }}
+            dropdownStyle={{ backgroundColor: colors.card, borderRadius: 12, padding: 15, height: 'auto' }}
+            renderDropdownIcon={() => <Icon name="chevron-down-outline" color={colors.text}></Icon>}
+            dropdownIconPosition='right'
+            data={['EASY', 'HARD']}
+            rowTextForSelection={(item) => {
+                return t(`meals.complexity.${item}`)
+            }}
+            buttonTextAfterSelection={(item) => {
+                return t(`meals.complexity.${item}`)
+            }}
+            onSelect={(selectedItem) => {
+                setComplex(selectedItem);
+            }}
+        />
 
-            <View style={{ alignItems: 'center' }}>
-                <Icon.Button
-                    style={{ paddingVertical: 15, paddingHorizontal: 25 }}
-                    name="add-outline"
-                    borderRadius={25}
-                    backgroundColor={colors.primary}
-                    onPress={() => navigation.navigate({
-                        name: 'List',
-                        params: { newMeal: { name: name, ingredients: [], complex: complex } },
-                        merge: true,
-                    })}
-                >
-                    {t('meals.save')}
-                </Icon.Button>
-            </View>
+        <View style={{ alignItems: 'center' }}>
+            <Icon.Button
+                style={{ paddingVertical: 15, paddingHorizontal: 25 }}
+                name="add-outline"
+                borderRadius={25}
+                backgroundColor={colors.primary}
+                onPress={() => navigation.navigate({
+                    name: 'List',
+                    params: { newMeal: { id: IdGenerator.generate(), name: name, ingredients: [], complexity: complex } },
+                    merge: true,
+                })}
+            >
+                {t('meals.save')}
+            </Icon.Button>
         </View>
-    </ScrollView >
+    </View>
 }
--- src/Plan.tsx
+++ src/Plan.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useTranslation } from "react-i18next";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { Section } from "./Section";
 
@@ -24,39 +24,34 @@ export function PlanList(): React.JSX.Element {
     const { colors } = useTheme();
     const { t } = useTranslation();
 
-    return <View style={{ height: '100%' }}>
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic">
-            <View
+    return <View
+        style={{
+            backgroundColor: colors.background,
+        }}>
+        <Section title={t('plan.introHeading')}>
+            <Text>{t('plan.introDescription')} </Text>
+        </Section>
+        <View style={{
+            alignItems: 'center',
+            gap: 20
+        }}>
+            <Image
+                source={require('./img/Farfalle.png')}
                 style={{
-                    backgroundColor: colors.background,
-                }}>
-                <Section title={t('plan.introHeading')}>
-                    <Text>{t('plan.introDescription')} </Text>
-                </Section>
-                <View style={{
-                    alignItems: 'center',
-                    gap: 20
-                }}>
-                    <Image
-                        source={require('./img/Farfalle.png')}
-                        style={{
-                            height: 380,
-                            resizeMode: 'contain',
-                            tintColor: colors.text,
-                        }}
-                    />
-                    <Icon.Button
-                        style={{ paddingVertical: 15, paddingHorizontal: 25 }}
-                        name="reload-outline"
-                        borderRadius={25}
-                        backgroundColor={colors.primary}
-                        onPress={() => console.log("Pressed")}
-                    >
-                        {t('plan.generate')}
-                    </Icon.Button>
-                </View>
-            </View>
-        </ScrollView>
-    </View>;
+                    height: 380,
+                    resizeMode: 'contain',
+                    tintColor: colors.text,
+                }}
+            />
+            <Icon.Button
+                style={{ paddingVertical: 15, paddingHorizontal: 25 }}
+                name="reload-outline"
+                borderRadius={25}
+                backgroundColor={colors.primary}
+                onPress={() => console.log("Pressed")}
+            >
+                {t('plan.generate')}
+            </Icon.Button>
+        </View>
+    </View>
 };
